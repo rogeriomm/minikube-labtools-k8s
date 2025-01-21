faead84960b1fe705369671e276e0f331aa6de3e
rogeriomm <rogermm@gmail.com>
2025-01-21 07:50:55 -0300

    Fix installer, update README,add missed files

---
+++ install/src/main.go
@@ -0,0 +1,270 @@
+/*
+
+ */
+package main
+
+import (
+	"bufio"
+	"context"
+	"flag"
+	"fmt"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	v1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+	"strconv"
+)
+
+type Bind9 struct {
+	f *os.File
+}
+
+func (bind *Bind9) open() {
+	var err error
+	bind.f, err = os.OpenFile("/usr/local/etc/bind/zones/db.worldl.xpt", os.O_APPEND|os.O_RDWR, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (bind *Bind9) close() {
+	bind.f.Close()
+}
+
+func (bind *Bind9) findBindZone(key string) bool {
+	scanner := bufio.NewScanner(bind.f)
+	r, err := regexp.Compile(key)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for scanner.Scan() {
+		if r.MatchString(scanner.Text()) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (bind *Bind9) updateZeppelinIngress(ip string) {
+	bind.open()
+	defer bind.close()
+	found := bind.findBindZone("\\$INCLUDE /usr/local/etc/bind/zones/zeppelin.worldl.xpt")
+	if !found {
+		if _, err := bind.f.WriteString("$INCLUDE /usr/local/etc/bind/zones/zeppelin.worldl.xpt\n"); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	f, err := os.OpenFile("/usr/local/etc/bind/zones/zeppelin.worldl.xpt", os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	f.WriteString("*.zeppelin.worldl.xpt. IN A " + ip + "\n")
+	f.WriteString("zeppelin.worldl.xpt. IN A " + ip + "\n")
+	f.Close()
+}
+
+func (bind *Bind9) updateK8sIngress(ip string) {
+	bind.open()
+	defer bind.close()
+	found := bind.findBindZone("\\$INCLUDE /usr/local/etc/bind/zones/ingress-k8s.worldl.xpt")
+	if !found {
+		if _, err := bind.f.WriteString("$INCLUDE /usr/local/etc/bind/zones/ingress-k8s.worldl.xpt\n"); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	f, err := os.OpenFile("/usr/local/etc/bind/zones/ingress-k8s.worldl.xpt", os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	f.WriteString("*.worldl.xpt. IN A " + ip + "\n")
+	f.Close()
+}
+
+type k8s struct {
+	core v1.CoreV1Interface
+}
+
+func (k *k8s) kubecfg() {
+	var kubeconfig *string
+
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"),
+			"(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	k.core = clientset.CoreV1()
+}
+
+func (k *k8s) getNodeIngress() string {
+	pod := k.core.Pods("ingress-nginx")
+	opts := metav1.ListOptions{
+		LabelSelector: "app.kubernetes.io/name=ingress-nginx",
+	}
+
+	p, err := pod.List(context.TODO(), opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return p.Items[0].Spec.NodeName
+}
+
+func (k *k8s) getIngressMinikube() string {
+	node := k.getNodeIngress()
+
+	n, err := k.core.Nodes().Get(context.TODO(), node, metav1.GetOptions{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ip := n.Status.Addresses[0].Address
+
+	return ip
+}
+
+func (k *k8s) getIngressZeppelin() string {
+	svc, err := k.core.Services("zeppelin").Get(context.TODO(), "zeppelin-server",
+		metav1.GetOptions{})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return svc.Spec.ClusterIP
+}
+
+func restartBind() {
+	log.Println("Restarting BIND...")
+	_, err := exec.Command("sudo", "brew", "services", "restart", "bind").Output()
+	//log.Println(string(out))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func flushDnsCache() {
+	log.Println("Flushing host DNS cache...")
+	_, err := exec.Command("dscacheutil", "-flushcache").Output()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func minikubeSsh(node string, parms string) {
+	out, err := exec.Command("minikube", "--node="+node, "ssh", parms).Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(out) == 0 {
+		return
+	}
+	fmt.Println(string(out))
+}
+
+func minikubeSetProfile(name string) {
+	_, err := exec.Command("minikube", "profile", name).Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func minikubeGetMainIp() string {
+	out, err := exec.Command("minikube", "ip").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(out)
+}
+
+func minikubeAddIpRoute() {
+	ip := minikubeGetMainIp()
+
+	out, err := exec.Command("sudo", "route", "-n", "add", "10.0.0.0/8", ip).Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(out))
+}
+
+func sudoValidateUser() {
+	_, err := exec.Command("sudo", "-v").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	k := &k8s{}
+	b := Bind9{}
+
+	log.Println("Minikube lab tool")
+	args := os.Args[1:]
+
+	if len(args) != 1 {
+		log.Println("Invalid argument")
+		return
+	}
+
+	if args[0] != "configure" {
+		log.Println("Invalid argument")
+		return
+	}
+
+	sudoValidateUser()
+
+	k.kubecfg()
+	ipIngressMinikube := k.getIngressMinikube()
+	log.Println("Minikube ingress ip:", ipIngressMinikube)
+
+	b.updateK8sIngress(ipIngressMinikube)
+
+	nodeList, err1 := k.core.Nodes().List(context.TODO(), metav1.ListOptions{})
+	if err1 != nil {
+		log.Fatal(err1)
+	}
+
+	log.Println("Running init script on Minikube nodes")
+	for _, node := range nodeList.Items {
+		minikubeSsh(node.Name, "[ -f init.sh ] && sudo sh init.sh")
+	}
+
+	log.Println("Creating storage on Minikube nodes")
+	var pv int64 = 1
+	for _, node := range nodeList.Items {
+		minikubeSsh(node.Name, "sudo mkdir -p /data/local-storage/pv000"+strconv.FormatInt(pv, 10))
+		pv++
+		minikubeSsh(node.Name, "sudo mkdir -p /data/local-storage/pv000"+strconv.FormatInt(pv, 10))
+		pv++
+	}
+
+	minikubeSetProfile("cluster2")
+
+	log.Println("Add Minikube IP route")
+	minikubeAddIpRoute()
+
+	restartBind()
+	flushDnsCache()
+}
