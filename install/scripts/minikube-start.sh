7d922929c2dfb8edd8bd0bfc56287a2323d1bf70
rogeriomm <rogermm@gmail.com>
2025-01-21 07:50:55 -0300

    minikube start script: handle error

--- install/scripts/minikube-start.sh
+++ install/scripts/minikube-start.sh
@@ -8,9 +8,12 @@ clusters_start
 
 sudo -v
 
-echo "Dig www.google.com"
-dig @192.168.64.1 www.google.com
-echo "Check MACOS firewall. Add named firewall rule and restart named"
+echo "Checking Minikube DNS"
+
+if ! dig @192.168.64.1 www.google.com; then
+  echo 'Check MACOS firewall. Add "named" firewall rule and restart named'
+  exit 2
+fi
 
 sudo -v
 
