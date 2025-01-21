a54e8592dccad38d931c35724c77848ff9bcfc32
rogeriomm <rogermm@gmail.com>
2025-01-21 07:50:54 -0300

    Improve minikube mount using 9P

--- scripts/minikube-mount.sh
+++ scripts/minikube-mount.sh
@@ -2,4 +2,11 @@
 
 PATH=$PATH:/usr/local/bin
 mac_ip=$(ifconfig bridge100 | grep "inet " | awk -F ' ' '{print $2}')
-minikube -p cluster2 mount --ip $mac_ip $HOME/git/dataops/bdmm/labtools-k8s:/labtools-k8s
+
+# On Minikube nodes run:
+# sudo mkdir -p /home/${USERNAME}
+# sudo mount -t 9p -o ro -o version=9p2000.L -o dfltuid=1000 -o dfltgid=1000 -o port=8888 ${mac_ip} /home/${USERNAME}
+
+minikube -p cluster2 mount --options fscache,ro --kill=false --ip $mac_ip --port=8888 --9p-version='9p2000.L' \
+    $HOME:/home/$USERNAME
+
