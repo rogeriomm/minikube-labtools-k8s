db2d4d8c20d804514cf981bdea7ed340854871bb
rogeriomm <rogermm@gmail.com>
2025-01-21 07:50:55 -0300

    Configure 2 Minikube clusters, each one using different versions

--- install/scripts/minikube-init.sh
+++ install/scripts/minikube-init.sh
@@ -3,11 +3,11 @@
 source minikube-lib.sh
 
 if [[ "$1" = "install" ]]; then
-  sudo echo -n
+  sudo -v
   are_you_sure
   init
 elif [[ "$1" = "postinstall" ]]; then
-  sudo echo
+  sudo -v
   post_init
 elif [[ "$1" = "argocd" ]]; then
   argocd_show_password
